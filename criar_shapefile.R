b1d2263d57479f0bf7dd180b53b39275b8efca52
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2026-04-16 20:29:29 -0300

    Criar a interface interativa dos pacote leaflet e leaflet.extras

--- criar_shapefile.R
+++ criar_shapefile.R
@@ -9,3 +9,17 @@ library(mapedit)
 library(sf)
 
 library(tidyverse)
+
+# Criar o shapefile ----
+
+## Ativar a interface interativa ----
+
+mapa <- leaflet::leaflet() |>
+  leaflet::addProviderTiles(provider = providers$Esri.WorldImagery) |>
+  leaflet.extras::addDrawToolbar(targetGroup = "draw",
+                                 polylineOptions = TRUE,
+                                 polygonOptions = TRUE,
+                                 circleOptions = TRUE,
+                                 rectangleOptions = TRUE,
+                                 markerOptions = TRUE,
+                                 editOptions = leaflet.extras::editToolbarOptions())
