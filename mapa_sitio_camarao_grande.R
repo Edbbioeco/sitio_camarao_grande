29e85661f18b22321b792a53ddc5fa4f96bbec84
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-01 12:14:23 -0300

    Filtrar e visualizar o shapefile de Pernambuco

--- mapa_sitio_camarao_grande.R
+++ mapa_sitio_camarao_grande.R
@@ -26,5 +26,23 @@ br <- geobr::read_state(year = 2019)
 
 ### Visualizar ----
 
+br
+
 ggplot() +
   geom_sf(data = br, color = "black")
+
+## Shapefile de Pernambuco ----
+
+### Filtrar ----
+
+pe <- br |>
+  dplyr::filter(abbrev_state == "PE")
+
+### Visualizar ----
+
+pe
+
+ggplot() +
+  geom_sf(data = br, color = "black") +
+  geom_sf(data = pe, color = "black", fill = "goldenrod")
+
