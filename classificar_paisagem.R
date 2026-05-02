f684e816c83be921aa21347c59387ac296c1e294
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-02 13:52:50 -0300

    Importar e visualizar o shapefile do Sítio Camarão Grande

--- classificar_paisagem.R
+++ classificar_paisagem.R
@@ -12,3 +12,19 @@ library(terra)
 
 library(randomForest)
 
+# Dados ----
+
+## Shapefile do Sítio Camarão Grande ----
+
+### Importar ----
+
+## Importar ----
+
+scg <- sf::st_read("shapefile_sitio.shp")
+
+## Visualizar ----
+
+scg
+
+ggplot() +
+  geom_sf(data = scg, color = "black")
