75fdcd3829818c4dd7c7bcf2eb5b2b73f5fa2eaa
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-02 12:01:22 -0300

    Mapa de visualização da área

--- pontos_treino_modelos_classificação_paisagem.R
+++ pontos_treino_modelos_classificação_paisagem.R
@@ -22,3 +22,24 @@ scg
 
 ggplot() +
   geom_sf(data = scg, color = "black")
+
+# Pontos de treino ----
+
+## Criar o mapa a ser editado ----
+
+mapa <- leaflet::leaflet() |>
+  leaflet::addProviderTiles(provider = providers$Esri.WorldImagery) |>
+  leaflet.extras::addDrawToolbar(targetGroup = "Draw",
+                                 polylineOptions = TRUE,
+                                 polygonOptions = TRUE,
+                                 circleOptions = TRUE,
+                                 rectangleOptions = TRUE,
+                                 markerOptions = TRUE,
+                                 circleMarkerOptions = TRUE,
+                                 editOptions = leaflet.extras::editToolbarOptions()) |>
+  leafem::addMouseCoordinates() |>
+  leaflet::addPolygons(data = scg,
+                       color = "gold",
+                       fillOpacity = 0)
+
+mapa
