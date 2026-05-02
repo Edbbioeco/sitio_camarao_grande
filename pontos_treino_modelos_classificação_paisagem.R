56f4f21f242d69864e38410745182445cb781d76
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-02 11:24:06 -0300

    Importar e visualizar o Shapefile do Sítio Camarão Grande

--- pontos_treino_modelos_classificação_paisagem.R
+++ pontos_treino_modelos_classificação_paisagem.R
@@ -7,3 +7,16 @@ library(leaflet)
 library(leaflet.extras)
 
 library(mapedit)
+
+# Shpefile do sítio ----
+
+## Importar ----
+
+scg <- sf::st_read("shapefile_sitio.shp")
+
+## Visualizar ----
+
+scg
+
+ggplot() +
+  geom_sf(data = scg, color = "black")
