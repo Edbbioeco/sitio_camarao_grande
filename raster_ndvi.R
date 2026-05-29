9e5d92c45e6296e034d379751da6f6801f608303
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-29 12:23:54 -0300

    Salvar catálogo em um data frame

--- raster_ndvi.R
+++ raster_ndvi.R
@@ -38,11 +38,15 @@ OAuthClient
 
 ## Catálogo ----
 
-CDSE::SearchCatalog(aoi = scg,
-                    from = "2020-01-01", to = "2026-05-01",
-                    collection = "sentinel-2-l2a",
-                    with_geometry = TRUE,
-                    client = OAuthClient)
+catalogo <- CDSE::SearchCatalog(aoi = scg,
+                                from = "2020-01-01", to = "2026-05-01",
+                                collection = "sentinel-2-l2a",
+                                with_geometry = FALSE,
+                                client = OAuthClient)
+
+catalogo
+
+catalogo |> dplyr::glimpse()
 
 ## Evalscript ----
 
