99948f1a311a2b0a8b3d3c1da8c98a975406533e
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-30 11:10:26 -0300

    Importar e visualizar o shapefile da área

--- raster_rgb.R
+++ raster_rgb.R
@@ -13,3 +13,16 @@ library(terra)
 library(tidyterra)
 
 library(ggview)
+
+# Shapefile da área ----
+
+## Importar ----
+
+scg <- sf::st_read("shapefile_sitio.shp")
+
+## Visualizar ----
+
+scg
+
+ggplot() +
+  geom_sf(data = scg, color = "black")
