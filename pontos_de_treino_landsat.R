bba908db98b1488fa5da30a3bf4af8513feb90ca
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-31 18:09:12 -0300

    Visualizar raster rgb

--- pontos_de_treino_landsat.R
+++ pontos_de_treino_landsat.R
@@ -36,3 +36,11 @@ ggplot() +
 ### Importar ----
 
 rster_rg <- terra::rast("rgb/rbg_2019-11-24.tif")
+
+### Visualizar ----
+
+rster_rg
+
+ggplot() +
+  tidyterra::geom_spatraster_rgb(data = rster_rg) +
+  geom_sf(data = scg, color = "red", fill = "transparent")
