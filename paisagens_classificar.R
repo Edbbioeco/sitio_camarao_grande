bca12e4c4bfc7630c3714a599a335c215257c1d5
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-31 10:36:47 -0300

    Importar rasters

--- paisagens_classificar.R
+++ paisagens_classificar.R
@@ -9,3 +9,14 @@ library(tidyterra)
 library(DescTools)
 
 library(ggview)
+
+# Rasters ----
+
+## Importar ----
+
+rasters <- purrr::map(list.files(path = "./rgb",
+                                 full.names = TRUE),
+                      terra::rast) |>
+  setNames(list.files(path = "./rgb",
+                      full.names = TRUE) |>
+             stringr::str_remove_all("./rgb/|.tif"))
