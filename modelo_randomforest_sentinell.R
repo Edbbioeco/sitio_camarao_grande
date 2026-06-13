0de898bf85c48560fbfab74fa5957ecf95e220fb
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-12 23:40:16 -0300

    Importar e visualizar o shapefile do sítio

--- modelo_randomforest_sentinell.R
+++ modelo_randomforest_sentinell.R
@@ -9,3 +9,18 @@ library(terra)
 library(tidyterra)
 
 library(randomForest)
+
+# Dados ----
+
+## Shapefile do sítio ----
+
+### Importar ----
+
+sitio <- sf::st_read("shapefile_sitio.shp")
+
+### Visualizar ----
+
+sitio
+
+ggplot() +
+  geom_sf(data = sitio)
