9d5db93c2915827e5239da07fa8e131cc2ba07ef
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-14 19:12:10 -0300

    Baixar rasters de uso e cobertura do solo entre 1985 a 2025

--- uso_cobertura_solo.R
+++ uso_cobertura_solo.R
@@ -24,3 +24,33 @@ sitio
 
 ggplot() +
   geom_sf(data = sitio, color = "black")
+
+# Baixar e recortar rasters de uso e cobertura do solo ----
+
+## Baixar ----
+
+raster_uso <- purrr::map(
+  1985:2025,
+  purrr::in_parallel(
+
+    tryCatch(
+      \(periodo){
+
+        download.file(
+         paste0("https://storage.googleapis.com/mapbiomas-public/initiatives/brasil/collection_10/lulc/coverage/brazil_coverage_",
+                periodo,
+                ".tif"),
+         "mapbiomas_local.tif",
+         mode = "wb")
+
+        terra::rast("mapbiomas_local.tif") |>
+          terra::crop(sitio) |>
+          terra::mask(sitio)
+
+        file.remove("mapbiomas_local.tif")
+
+    },
+    error = \(e){})
+
+    ),
+  .progress = TRUE)
