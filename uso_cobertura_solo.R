e64fa9e2c8556ec0215adbff12917fc42e791ee8
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-14 21:13:48 -0300

    Exportar mapas

--- uso_cobertura_solo.R
+++ uso_cobertura_solo.R
@@ -92,6 +92,57 @@ purrr::imap(raster_uso_trat,
            ),
            .progress = TRUE)
 
+## Exportar os mapas ----
+
+mapas_uso <- purrr::imap(raster_uso_trat,
+                         purrr::in_parallel(
+
+             ~ggplot() +
+               tidyterra::geom_spatraster(data = .x) +
+               scale_fill_viridis_c(option = "turbo",
+                                    na.value = "transparent",
+                                    guide = guide_colourbar(
+
+                                      title.position = "top",
+                                      title.hjust = 0.5,
+                                      barwidth = 20,
+                                      frame.colour = "black",
+                                      ticks.colour = "black")
+
+                                    ) +
+               labs(title = paste0("Uso e cobertura do siolo para ", .y),
+                    subtitle = "Fonte: MapBiomas",
+                    fill = "Classe de uso e cobertura do solo") +
+               theme_bw() +
+               theme(axis.text = element_text(size = 20, color = "black"),
+                     legend.text = element_text(size = 20, color = "black"),
+                     legend.title = element_text(size = 20, color = "black"),
+                     legend.position = "bottom",
+                     strip.text = element_text(size = 30, color = "black"),
+                     strip.background = element_rect(color = "black",
+                                                     linewidth = 1),
+                     panel.background = element_rect(linewidth = 1,
+                                                     color = "black"),
+                     plot.title = element_text(size = 20, color = "black"),
+                     plot.subtitle = element_text(size = 17.5, color = "black"))
+
+                         ),
+                         .progress = TRUE)
+
+mapas_uso
+
+purrr::imap(mapas_uso,
+            purrr::in_parallel(
+
+             ~ggsave(.x,
+                     filename = paste0("./mapas_uso_cobertura_solo/mapa_",
+                                       .y,
+                                       ".png"),
+                     height = 10, width = 12)
+
+             ),
+            .progress = TRUE)
+
 # Diversidade da paisagem ----
 
 ## Calcular diversidade ----
