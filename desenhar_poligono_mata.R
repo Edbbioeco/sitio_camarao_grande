48676c4d5efe1c2362acf0b6d30341d6e2676e1b
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-16 10:44:12 -0300

    Importar e visualizar o shapefile do Sítio Camarão Grande

--- desenhar_poligono_mata.R
+++ desenhar_poligono_mata.R
@@ -11,3 +11,16 @@ library(leaflet.extras)
 library(leafem)
 
 library(mapedit)
+
+# Shapefile do sítio Camaraão Grande ----
+
+## Importar ----
+
+scg <- sf::st_read("shapefile_sitio.shp")
+
+## Visualizar ----
+
+scg
+
+ggplot() +
+  geom_sf(data = scg, color = "black")
