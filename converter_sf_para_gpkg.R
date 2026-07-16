e5bedf7f3b3d1b078c8fa5bfd462d0a8e671a3e6
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-16 12:48:01 -0300

    Converter .shp para.gpkg

--- converter_sf_para_gpkg.R
+++ converter_sf_para_gpkg.R
@@ -12,3 +12,20 @@ sfs <- list.files(pattern = ".shp$",
                   full.names = TRUE)
 
 sfs
+
+## Converter ----
+
+purrr::map(sfs,
+           purrr::in_parallel(
+             \(shp){
+
+               sf <- sf::st_read(shp)
+
+               sf |> sf::st_write(paste0("./",
+                                         shp |>
+                                           stringr::str_remove_all("./|.shp"),
+                                         ".gpkg"))
+
+               }
+           ),
+           .progress = TRUE)
